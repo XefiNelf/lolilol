63eb34f427579a9ff87b584a7e20b29c3e0b2501
Denis Zaratan Pasin <zaratan@hey.com>
2021-05-27 15:28:43 +0200

    first commi

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn app:lol
+web: gunicorn app:main.py
---
+++ main.py
@@ -0,0 +1,16 @@
+from flask import Flask, url_for, session
+from flask import render_template, redirect
+from authlib.integrations.flask_client import OAuth
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def homepage():
+    
+    return "<h1>koukou</h1>"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=os.environ.get("PORT", 80))
